41827b8f0e90827727a8df5a34b14132a4dba62f
GFreire191 <guilherme.lm.freire@gmail.com>
2024-12-06 19:23:53 +0000

    FrontEnd NavBar

--- musex_frontend/src/app/app.routes.ts
+++ musex_frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {HomePageComponent} from './components/home-page/home-page.component';
+import {RegisterComponent} from './register/register.component';
 
 export const routes: Routes = [
   {path: '', component: HomePageComponent},
+  {path: 'register', component: RegisterComponent},
 ];
