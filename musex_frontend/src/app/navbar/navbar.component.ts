e9a6d087ba0b9408e53343dbc42bf8ef697507e0
GFreire191 <guilherme.lm.freire@gmail.com>
2024-12-11 01:49:10 +0000

    NavBarCss

--- musex_frontend/src/app/navbar/navbar.component.ts
+++ musex_frontend/src/app/navbar/navbar.component.ts
@@ -2,17 +2,37 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, Validators,ReactiveFormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink,RouterLinkActive,CommonModule],
+  imports: [RouterLink,RouterLinkActive,CommonModule,ReactiveFormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   userId: string | null;
-  constructor(public authService: AuthService) {
+  searchForm: FormGroup;
+  constructor(public authService: AuthService, private router: Router, private fb: FormBuilder) {
+
 
     this.userId = this.authService.getUserId();
+    this.searchForm = this.fb.group({
+      search: ['']
+    });
+
+
   }
+
+
+  onSubmit() {
+    if (this.searchForm.invalid) {
+      return;
+    }
+    this.router.navigate(['/search'], { queryParams: { q: this.searchForm.value.search } });
+  }
+
+
 }
